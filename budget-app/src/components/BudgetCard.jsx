c6194c564aaf7b850696288ee77d3f8dc651918e
Rick Moore <rick329mo@gmail.com>
2022-12-04 13:10:57 -0600

    created a danger effect when budget reached

--- budget-app/src/components/BudgetCard.jsx
+++ budget-app/src/components/BudgetCard.jsx
@@ -1,9 +1,16 @@
 import { Button, Card, ProgressBar, Stack } from "react-bootstrap"
 import { currencyFormatter } from "../utils"
 
-function BudgetCard({ name, amount, max }) {
+function BudgetCard({ name, amount, max, gray }) {
+    const classnames = []
+    if(amount > max) {
+        classnames.push("bg-danger", "bg-opacity-10")
+    } else if(gray) {
+        classnames.push("bg-light")
+    }
+    
     return(
-        <Card>
+        <Card className={classnames.join(" ")}>
             <Card.Body>
                 <Card.Title className="d-flex justify-content-between
                 align-items-baseline fw-normal mb-3">
