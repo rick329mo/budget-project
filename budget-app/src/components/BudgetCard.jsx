5c22ea8ea15676fbdb0bb5cb5a25191145ff4422
Rick Moore <rick329mo@gmail.com>
2022-12-04 12:51:49 -0600

    added progress bar and variant

--- budget-app/src/components/BudgetCard.jsx
+++ budget-app/src/components/BudgetCard.jsx
@@ -1,17 +1,35 @@
-import { Card } from "react-bootstrap"
+import { Card, ProgressBar } from "react-bootstrap"
 import { currencyFormatter } from "../utils"
 
 function BudgetCard({ name, amount, max }) {
     return(
         <Card>
             <Card.Body>
-                <Card.Title>
-                    <div>{name}</div>
-                    <div>{currencyFormatter.format(amount)} / {currencyFormatter.format(max)}</div>
+                <Card.Title className="d-flex justify-content-between
+                align-items-baseline fw-normal mb-3">
+                    <div className="me-2">{name}</div>
+                    <div className="d-flex align-items-baseline">
+                        {currencyFormatter.format(amount)}
+                        <span className="text-muted fs-6 ms-1">/ {currencyFormatter.format(max)}</span>
+                    </div>
                 </Card.Title>
+                <ProgressBar 
+                    className="rounded-pill" 
+                    variant={getProgressBarVariant(amount, max)}
+                    min={0}
+                    max={max}
+                    now={amount}
+                />
             </Card.Body>
         </Card>
     )
+}
+
+function getProgressBarVariant(amount, max) {
+    const ratio = amount / max
+    if(ratio < .5) return "primary"
+    if(ratio < .75) return "warning"
+    return "danger"
 }
 
 export default BudgetCard
