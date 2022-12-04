f6ae728cf96478223a95981f67cf184269ba5e1e
Rick Moore <rick329mo@gmail.com>
2022-12-04 14:33:57 -0600

    fixed addBudget, created addExpense
    added name checker, passed in name & max for addBudget.

--- budget-app/src/contexts/BudgetsContext.jsx
+++ budget-app/src/contexts/BudgetsContext.jsx
@@ -29,12 +29,19 @@ export const BudgetsProvider = ({ children }) => {
     function getBudgetExpenses(budgetId) {
         return expenses.filter(expense => expense.budgetId === budgetId)
     }
-    function addExpense() {
 
+    function addExpense({ description, amount, budgetId }) {
+        setExpenses(prevExpenses => {
+            return [...prevExpenses, { id: uuidV4(), description, amount, budgetId}]
+        })
     }
-    function addBudget() {
+    
+    function addBudget({ name, max }) {
         setBudgets(prevBudgets => {
-            return [...prevBudgets, { id: uuidV4()}]
+            if(prevBudgets.find(budget => budget.name === name)) {
+                return prevBudgets
+            }
+            return [...prevBudgets, { id: uuidV4(), name, max }]
         })
     }
     function deleteBudget() {
