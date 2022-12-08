58b3ea0cf52b279da2a4d619f9105261b48554bf
Rick Moore <rick329mo@gmail.com>
2022-12-08 14:03:00 -0600

    created viewExpensesModal body

--- budget-app/src/components/ViewExpensesModal.jsx
+++ budget-app/src/components/ViewExpensesModal.jsx
@@ -1,10 +1,12 @@
 import { Modal, Button, Stack } from "react-bootstrap"
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contexts/BudgetsContext"
+import { currencyFormatter } from "../utils"
 
 export default function ViewExpensesModal({ budgetId, handleClose}) {
     
     const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } = useBudgets()
 
+    const expenses =getBudgetExpenses(budgetId)
     const budget = 
         UNCATEGORIZED_BUDGET_ID === budgetId
             ? { name: "Uncategorized", id: UNCATEGORIZED_BUDGET_ID }
@@ -31,6 +33,16 @@ export default function ViewExpensesModal({ budgetId, handleClose}) {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <Stack direction="vertical" gap="3">
+                        {expenses.map(expense => (
+                            <Stack direction="horizontal" gap="2" key={expense.id}>
+                                <div className="me-auto fs-4">{expense.description}</div>
+                                <div className="fs-5">{currencyFormatter.format(expense.amount)}</div>
+                                <Button size="sm" variant="outline-danger">&times;</Button>
+
+                            </Stack>
+                        ))}
+                    </Stack>
                 </Modal.Body>
         </Modal>
     )
